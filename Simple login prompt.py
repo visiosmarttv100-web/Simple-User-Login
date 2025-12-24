aa55311bae0c63321b323cb907352b9d65815c11
visiosmarttv100-web <visiosmarttv100@gmail.com>
2025-12-24 21:37:29 +0100

    Add files via upload

---
+++ Simple login prompt.py
@@ -0,0 +1,28 @@
+print("Welcome!");
+
+username = input("Your username : ");
+
+password = input("Your password : ");
+
+if len(password) < 8 and password != "admin": 
+   print("8 Characters needed!");
+      
+else:
+   if username == "admin" and password =="admin":
+            print(f"Welcome {username}");
+   else:
+         print(f"Welcome {username}");
+           
+  
+
+   
+
+
+
+
+
+       
+ 
+
+
+       
